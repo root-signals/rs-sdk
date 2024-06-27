e90452312cbf58579bfbbcff6d2878a97cf79e6f
Markus Stenberg <markus.stenberg@rootsignals.ai>
2024-06-27 13:54:01 +0300

    sdk: Provide __version__

--- src/root/__init__.py
+++ src/root/__init__.py
@@ -9,6 +9,9 @@ Example::
 
 """
 
+from .__about__ import __version__
 from .client import RootSignals
 
-__all__ = ["RootSignals"]
+# Note: PEP-396 was rejected but we provide __version__ anyway
+# ( https://peps.python.org/pep-0396/ )
+__all__ = ["__version__", "RootSignals"]
