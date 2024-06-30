107f4717542c7aab89dfc34c80c6e6bf01978191
Ouz <oguzhan.gencoglu@ekahau.com>
2024-07-01 01:22:34 +0300

    minimal and simple updated

--- examples/simple.py
+++ examples/simple.py
@@ -5,24 +5,41 @@ client = RootSignals()
 
 # Create a skill
 skill = client.skills.create(
-    name="My cooking assistant",
-    intent="This skill will find you a recipe for a specific type of food.",
-    prompt="Find me good recipes for {{food_type}} food that are {{cuisine}}.",
+    name="My text classifier",
+    intent="To classify text into arbitrary categories based on semantics",
+    prompt="""
+    Classify this text into one of the following: {{categories}}
+    Text: {{text}}
+    """,
     model="gpt-4",
 )
 
-# Run it
-response = skill.run({"food_type": "spicy", "cuisine": "Korean"})
+# Execute
+response = skill.run(
+    {
+    "text": "The expectation for rate cuts has been steadily declining.",
+    "categories": "Finance, Sports, Politics"
+    }
+)
 print(response)
-# llm_output='1. Spicy Korean Chicken Stew ..'
-# validation={'validator_results': [], 'is_valid': True}
-# model='gpt-4' engine='gpt-4'
-# execution_log_id='102fcb9b-c692-48c6-adc4-c0c729d8b360'
-# rendered_prompt='Find me good recipes for spicy food that are Korean.'
+# "llm_output": "Finance",
+# "validation": Validation(is_valid=True, validator_results=[]),
+# "model": "gpt-4",
+# "execution_log_id": "9b3c713d-7bdc-4f7d-a85c-ed7d92ff4a56",
+# "rendered_prompt": (
+#     "Classify this text into one of the following: Finance, Sports, Politics\n"
+#     "Text: The expectation for rate cuts has been steadily declining."
+# ),
+# "cost": 0.00093,
 
 # We can retrieve the skill by id
 skill_2 = client.skills.get(skill_id=skill.id)
-response = skill_2.run({"food_type": "spicy", "cuisine": "Korean"})
+response = skill_2.run(
+    {
+    "text": "The expectation for rate cuts has been steadily declining.",
+    "categories": "Finance, Sports, Politics"
+    }
+)
 
 # We can also retrieve it by name
 # (the list result is an iterator, so we just take first one)
@@ -30,7 +47,11 @@ response = skill_2.run({"food_type": "spicy", "cuisine": "Korean"})
 # The name is not an unique identifier. Consequently, the .run method is not
 # intentionally available. However, you can circumvent this restriction if you
 # wish by using:
-skill_3 = next(client.skills.list(name="My cooking assistant"))
+skill_3 = next(client.skills.list(name="My text classifier"))
 response = client.skills.run(
-    skill_3.id, {"food_type": "spicy", "cuisine": "Korean"}
+    skill_3.id,
+    {
+    "text": "The expectation for rate cuts has been steadily declining.",
+    "categories": "Finance, Sports, Politics"
+    }
 )
