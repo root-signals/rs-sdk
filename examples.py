4c64edf8024ecf8ab88b47588cee31b5ae4487b4
Juho <155626310+juho-y@users.noreply.github.com>
2024-10-15 11:38:47 +0300

    Merge pull request #958 from root-signals/remove-test-environment-no-mock
    Remove TEST_ENVIRONMENT_NO_MOCK and related code

--- examples.py
+++ examples.py
@@ -58,7 +58,7 @@ def update_readme() -> bool:
     return False
 
 
-def generate_pytest(tests_requiring_llm) -> None:
+def generate_pytest() -> None:
     # This produces Python which doesn't quite handle our coding
     # style, but we have Ruff for that
     print("""# Automatically generated pytest definition, please do not edit
@@ -69,8 +69,6 @@ from root import RootSignals
     skip_line_match = re.compile(r"^(\s*#.*|from root import RootSignals|\s*)$").match
     for example_path in EXAMPLE_DIR.glob("*.py"):
         print("")
-        if example_path.stem in (tests_requiring_llm or []):
-            print("@pytest.mark.requires_llm")
         print(f"def test_{example_path.stem}():")
         for line in example_path.read_text().split("\n"):
             line = line.rstrip()
@@ -93,10 +91,9 @@ if __name__ == "__main__":
     p.add_argument(
         "--generate-pytest", "-t", action="store_true", help="Generate test module with contents of examples"
     )
-    p.add_argument("--requires-llm", "-l", nargs="+", help="Specify one or more tests that require an LLM")
     args = p.parse_args()
     if args.update_readme:
         if not update_readme():
             sys.exit(1)
     if args.generate_pytest:
-        generate_pytest(args.requires_llm)
+        generate_pytest()
